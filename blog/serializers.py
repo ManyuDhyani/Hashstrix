8421ee3a3147451a45eecafbc70618b8fe1016f2
ManyuDhyani <manyudhyani@gmail.com>
2022-02-24 10:56:39 +0530

    Merged the Author and Post model Serializer

--- blog/serializers.py
+++ blog/serializers.py
@@ -1,12 +1,14 @@
 from rest_framework import serializers
 from .models import Post, Author
 
-class PostSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Post
-        fields = '__all__'
-        
 class AuthorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Author
         fields = ['id', 'profile_picture', 'verified']
+
+class PostSerializer(serializers.ModelSerializer):
+    author = AuthorSerializer(read_only=True)
+    class Meta:
+        model = Post
+        fields = ['id', 'title', 'overview', 'timestamp', 'content', 'thumbnail', 'category', 'author']
+
